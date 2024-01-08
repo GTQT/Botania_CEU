bf127c5da774755b06540b10bca585e47d342741
KeQing <1158677939@qq.com>
2024-01-08 18:16:27 +0800

    原始提交

---
+++ src/main/java/com/leomeh/flowerpower/MyMod.java
@@ -0,0 +1,88 @@
+package com.leomeh.flowerpower;
+
+import com.leomeh.flowerpower.metatileentities.ModMetaTileEntities;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import zone.rong.mixinbooter.ILateMixinLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mod(
+        modid = "botaniaceu",
+        name="BotaniaCeu",
+        acceptedMinecraftVersions = "[1.12.2,1.13)",
+        version = "0.0.1-beta" ,
+        dependencies = "required-after:gregtech@[2.7.4-beta,) ;"
+)
+public class MyMod implements ILateMixinLoader {
+    public static final String MODID = "botaniaceu";
+    public static final String NAME = "BotaniaCeu";
+    public static final String VERSION = "1.0";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    @EventHandler
+    // preInit "Run before anything else. Read your config, create blocks, items, etc. (Remove if not needed)
+    public void preInit(FMLPreInitializationEvent event) {
+        // register to the event bus so that we can listen to events
+        ModMetaTileEntities.init();
+    }
+
+    @SubscribeEvent
+    // Register recipes here (Remove if not needed)
+    public void registerRecipes(RegistryEvent.Register<IRecipe> event) {
+
+    }
+
+    @SubscribeEvent
+    // Register items here (Remove if not needed)
+    public void registerItems(RegistryEvent.Register<Item> event) {
+
+    }
+
+    @SubscribeEvent
+    // Register blocks here (Remove if not needed)
+    public void registerBlocks(RegistryEvent.Register<Block> event) {
+
+    }
+
+    @EventHandler
+    // load "Do your mod setup. Build whatever data structures you care about." (Remove if not needed)
+    public void init(FMLInitializationEvent event) {
+    }
+
+    @EventHandler
+    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+
+    @EventHandler
+    // register server commands in this event handler (Remove if not needed)
+    public void serverStarting(FMLServerStartingEvent event) {
+    }
+
+    @Override
+    public List<String> getMixinConfigs() {
+        List<String> list = new ArrayList<>();
+        list.add("mixins.flowerpower.json");
+        list.add("mixins.mymodid.json");
+        return list;
+    }
+
+    @Override
+    public void onMixinConfigQueued(String mixinConfig) {
+        ILateMixinLoader.super.onMixinConfigQueued(mixinConfig);
+    }
+}
